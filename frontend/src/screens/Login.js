16b2ec9c01b152ac6ffc457a79ed5643ee2dcafe
Upendra <dhamalaupendra@gmail.com>
2021-04-14 15:18:00 +0545

    Developed Login Form

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import Counter from '../components/Counter'
+import './Login.css'
 const Login = () => {
-    return (
-        <div>
-            <Counter/>
-            Welcome to Login Page.
+  return (
+    <div className='container'>
+      <div className='layout'>
+        <h1>Sign In</h1>
+
+        <input className='form-field' type='text' placeholder='Username' />
+        <input className='form-field' type='text' placeholder='Password' />
+        <div className='remember-me'>
+          <input type='checkbox' id='check' />
+          <label htmlFor='check'>Remember me</label>
         </div>
-    )
+        <button className='btn' type='submit'>
+          Login
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Login
